185cbef7182ee28c4185f9dbbca4d27ad7c65803
ludolpr <ludolpr@gmail.com>
2024-07-30 10:49:55 +0200

    jwt + role

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,9 @@ class User extends Authenticatable implements JWTSubject
 
     public function getJWTCustomClaims()
     {
-        return [];
+        return [
+            'role' => $this->roles
+        ];
     }
 
     public function roles()
